f40c4294810f5e367ef9eb3710f6a2252202b70c
asrar <aliasrar84@gmail.com>
2023-10-31 19:49:18 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// let mone = document.getElementById("mone");
+// let mtwo = document.getElementById("mtwo");
+
+
+// let schedules1 = document.querySelector(".schedules1");
+// let schedules2 = document.querySelector(".schedules2");
+
+
+
+// mone.addEventListener("click", () => {
+//      schedules2.classList.add("display")
+// })
+
+// mtwo.addEventListener("click", () => {
+//     schedules1.classList.add("display")
+// })
